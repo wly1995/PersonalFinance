7963cf81bc3f580b504bb76e571a5bad0e17f65b
wly1995 <550612304@qq.com>
2017-03-16 20:57:04 +0800

    Picasso进行图片请求以及图片变圆形

--- app/src/main/java/com/atguigu/giugufinance/fragment/ProperyFragment.java
+++ app/src/main/java/com/atguigu/giugufinance/fragment/ProperyFragment.java
@@ -1,30 +1,94 @@
 package com.atguigu.giugufinance.fragment;
 
 import android.graphics.Color;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.atguigu.giugufinance.R;
+import com.atguigu.giugufinance.activity.MainActivity;
+import com.atguigu.giugufinance.bean.UserInfo;
+import com.atguigu.giugufinance.command.AppNetConfig;
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import jp.wasabeef.picasso.transformations.BlurTransformation;
+import jp.wasabeef.picasso.transformations.ColorFilterTransformation;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 /**
  * Created by 万里洋 on 2017/3/10.
  */
 
-public class ProperyFragment extends Fragment {
+public class ProperyFragment extends BaseFragment {
 
-    @Nullable
+
+    @Bind(R.id.tv_settings)
+    TextView tvSettings;
+    @Bind(R.id.iv_me_icon)
+    ImageView ivMeIcon;
+    @Bind(R.id.rl_me_icon)
+    RelativeLayout rlMeIcon;
+    @Bind(R.id.tv_me_name)
+    TextView tvMeName;
+    @Bind(R.id.rl_me)
+    RelativeLayout rlMe;
+    @Bind(R.id.recharge)
+    ImageView recharge;
+    @Bind(R.id.withdraw)
+    ImageView withdraw;
+    @Bind(R.id.ll_touzi)
+    TextView llTouzi;
+    @Bind(R.id.ll_touzi_zhiguan)
+    TextView llTouziZhiguan;
+    @Bind(R.id.ll_zichan)
+    TextView llZichan;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("资产");
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(20);
-        textView.setTextColor(Color.RED);
-//        View view = View.inflate(getActivity(), R.layout.fragment_home,null);
-        return textView;
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData(String json) {
+        MainActivity activity = (MainActivity) getActivity();
+        UserInfo user = activity.getUser();
+        //设置用户名
+        tvMeName.setText(user.getData().getName());
+
+        //设置头像
+       /* Picasso.with(getActivity())
+                .load(AppNetConfig.BASE_URL+"/images/tx.png")
+                .transform(new Transformation() {
+                    @Override
+                    public Bitmap transform(Bitmap bitmap) {
+                        Bitmap circleBitmap = BitmapUtils.circleBitmap(bitmap);
+                        bitmap.recycle(); //必须把原来的释放掉
+                        return circleBitmap;
+                    }
+                    @Override
+                    public String key() {
+                        return ""; //不能为空否则会报错
+                    }
+                })
+                .into(ivMeIcon);*/
+        Picasso.with(getActivity()).load(AppNetConfig.BASE_URL+"/images/tx.png")
+                .transform(new CropCircleTransformation())
+                .transform(
+                        new ColorFilterTransformation(Color
+                                .parseColor("#66FFccff")))
+                //第二个参数值越大越模糊
+                .transform(new BlurTransformation(getActivity(),10))
+                .into(ivMeIcon);
+    }
+
+    @Override
+    public int getLayoutid() {
+        return R.layout.fragment_property;
+    }
+
+    @Override
+    public String getChildUrl() {
+        return null;
     }
 }
